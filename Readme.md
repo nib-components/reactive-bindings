3f971cbcdb4afc4c03baf859babe3b675cac4a90
chrisbuttery <info@chrisbuttery.com>
2014-03-29 14:50:47 +1100

    1.0.0 - updated to work with reactive 1.0.x

--- Readme.md
+++ Readme.md
@@ -2,6 +2,8 @@
 # reactive-bindings
 
   Custom reactive bindings
+  
+  **Note**: Version 1.0.0 of reactive-bindings is to be used with **component/reactive 1.0.x**
 
 ## Installation
 
@@ -11,8 +13,69 @@
 
 ## API
 
+###data-model
 
+Bind an attribute to an input, select or textarea field.
+When the field updates, it updates the model. 
+When the model updates, the field state updates.
+
+The Template
+
+	<form class="js-skater">
+	  <input type="text" name="name" data-model="name">
+	  <input type="text" name="trick" data-model="trick">
+	  <label>
+	    <input name="isGnarly" type="radio" value="yes" data-model="isGnarly">Yes
+	  </label>
+	  <label>
+	    <input name="isGnarly" type="radio" value="no" data-model="isGnarly">No
+	  </label>
+	</form>
+
+Script
+
+	var reactive = require('reactive');
+	var bindings = require('reactive-bindings');
+	
+	var tmp = document.querySelector('.js-skater');
+	var model = {
+	  "name": "Tony Hawk";
+	  "trick" "900",
+	  "isGnarly": "yes"
+	}
+	
+	reactive(model, tmp, bindings);
+
+###data-option-text
+
+Set the text/value for a set of options in a select menu.
+The property value should be an array of strings that will be used to create and populate the options in a select field.
+
+The Template
+
+	<div class="js-sponsors">
+	  {name} sponsors included <select data-option-text="sponsors"></select>
+	</div>
+
+Script
+
+	var reactive = require('reactive');
+	var bindings = require('reactive-bindings');
+	
+	var tmp = document.querySelector('.js-sponsors');
+	var model = {
+	  "name": "Tony Hawk";
+	  "sponsors": ["Powell Peralta", "Independent", "Birdhouse Projects"]
+	}
+	
+	reactive(model, tmp, bindings);
 
 ## License
 
   MIT
+
+<script src="http://yandex.st/highlightjs/7.3/highlight.min.js"></script>
+<link rel="stylesheet" href="http://yandex.st/highlightjs/7.3/styles/github.min.css">
+<script>
+  hljs.initHighlightingOnLoad();
+</script>
--- index.js
+++ index.js
@@ -1,106 +1,78 @@
 var events = require('event');
 var value = require('value');
 
-module.exports = function(reactive) {
 
-  /**
-   * Hide the element when an attribute is truthy
-   * data-hidden="<attr>"
-   */
+/**
+ * Bind an attribute to an input, select or textarea field
+ * When the field updates, it updates the model. When the
+ * model updates, the field state updates
+ * <input data-model="email" name="email">
+ */
 
-  reactive.bind('data-hidden', function(el, value, obj) {
-    this.change(function(val){
-      if (val || val === true) {
-        el.classList.add('is-hidden');
-      } else {
-        el.classList.remove('is-hidden');
-      }
-    });
+
+function dataModel(el, property){   //el, attr
+  var binding = this;
+  var reactive = binding.reactive;
+  var type = el.getAttribute('type');
+  var name = el.nodeName.toLowerCase();
+
+  // When the field changes
+  events.bind(el, 'change', function(){
+    reactive.set(property, value(el));
   });
 
-  /**
-   * Show an element when an attribute is truthy
-   * data-visible="<attr>"
-   */
+  // When the attribute changes
+  binding.change(function() {
+    var val = reactive.get(property);
 
-  reactive.bind('data-visible', function(el, value, obj) {
-    this.change(function(val){
-      if (!val || val === false) {
-        el.classList.add('is-hidden');
-      } else {
-        el.classList.remove('is-hidden');
-      }
-    });
-  });
-
-  /**
-   * Set the text value for a set of options in a select menu.
-   * The attribute value should be an array of strings that will
-   * be used to populate the content of the options in a select
-   * data-option-text="<attr>"
-   */
-
-  reactive.bind('data-option-text', function(el, value, obj) {
-    this.change(function(options){
-      var options = obj.get(value);
-      Array.prototype.forEach.call(el.querySelectorAll('option'), function(option, i){
-        option.innerHTML = options[i];
-      });
-    });
-  });
-
-  /**
-   * Bind an attribute to an input, select or textarea field
-   * When the field updates, it updates the model. When the
-   * model updates, the field state updates
-   * data-model="email"
-   */
-
-  reactive.bind('data-model', function(el, attr, model) {
-    var type = el.getAttribute('type');
-    var name = el.nodeName.toLowerCase();
-
-    // When the field changes
-    events.bind(el, 'change', function(){
-      model.set(attr, value(el));
-    });
-
-    // When the attribute changes
-    this.change(function(){
-      var val = model.get(attr);
-      if(val == null) {
-        val = "";
-      }
-      if(name !== "input" && name !== "select") {
-        el.innerHTML = val;
-      }
-      else if(type === "radio") {
-        value(el, el.value === String(val));
-      }
-      else {
-        value(el, val);
-      }
-    });
-
-    // Fill the model with the data immediately
-    // if there is no value on the model already
-    if(model.get(attr) == null) {
-      model.set(attr, value(el));
+    if(val == null) {
+      val = "";
     }
-
+    if(name !== "input" && name !== "select") {
+      el.innerHTML = val;
+    }
+    else if(type === "radio") {
+      value(el, el.value === String(val));
+    }
+    else {
+      value(el, val);
+    }
   });
 
-  /**
-   * Parse JSON within an element and set it on the model.
-   * This is usually when the backend wants to prefill the
-   * model data using a script tag with JSON.
-   */
+  // Populate the reactive model immediately, if there is no value already
+  if(reactive.get(property) == null) {
+    reactive.set(property, value(el));
+  }
+}
 
-  reactive.bind('data-json', function(el, value, model) {
-    var data = JSON.parse(el.innerHTML);
-    var parsed = model.parse(data);
-    model.set(parsed);
-    el.parentNode.removeChild(el);
+
+/**
+ * Set the text/value for a set of options in a select menu.
+ * The property value should be an array of strings that will
+ * be used to create and populate the options in a select
+ * <select data-option-text="{name}"></select>
+ */
+
+
+function optionText(el, property){
+  var binding = this;
+  binding.change(function() {
+    var options = binding.value(property);
+    for(var i=0, n = options.length; i < n; i++){
+      var option = document.createElement('option');
+      option.value = option.text = options[i];
+      el.appendChild(option);
+    }
   });
+}
 
+/**
+ * Expose `bindings`.
+ */
+
+module.exports = {
+  bindings: {
+    'data-option-text': optionText,
+    'data-model': dataModel
+  }
 };
